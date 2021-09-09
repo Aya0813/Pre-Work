355baf056fb96022ba004dd5649de28a00c34cbb
Aya0813 <ae2195@nyu.edu>
2021-09-09 20:10:33 +0400

    Tip Calculator App
    Final submission of Pre-Work Tip Calculator App

---
+++ Pre-Work (Tip Calculator App)Tests/Pre_Work__Tip_Calculator_App_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Pre_Work__Tip_Calculator_App_Tests.swift
+//  Pre-Work (Tip Calculator App)Tests
+//
+//  Created by mac on 07/09/2021.
+//
+
+import XCTest
+@testable import Pre_Work__Tip_Calculator_App_
+
+class Pre_Work__Tip_Calculator_App_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
